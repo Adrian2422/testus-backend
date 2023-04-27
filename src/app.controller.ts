66eedf14903045a45ee422cdd3a61ca77c41a18b
Adrian_Kloc <adrian.kloc47@gmail.com>
2023-04-27 23:12:34 +0200

    Swagger implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor() {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@ApiExcludeEndpoint()
+	@Get()
+	index() {
+		return 'Welcome to Testus api!';
+	}
 }
