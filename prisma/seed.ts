9bb3a91bba1575bfa0f293eaa7b060919f1597ec
Adrian_Kloc <adrian.kloc47@gmail.com>
2023-04-27 22:05:12 +0200

    Docker and prisma setup

---
+++ prisma/seed.ts
@@ -0,0 +1,79 @@
+import * as bcrypt from 'bcryptjs';
+import { faker } from '@faker-js/faker';
+import {
+	PrismaClient,
+	UserRoles
+} from '@prisma/client';
+
+const randomUsersCount = 100;
+const prisma = new PrismaClient();
+
+function hashPassword(password: string): Promise<string> {
+	const saltRounds = 10;
+	return bcrypt.hash(password, saltRounds);
+}
+
+async function main(): Promise<void> {
+	await prisma.user.deleteMany();
+
+	console.log('Seeding...');
+
+	generateAdmin()
+		.then(() => generateUsers())
+		.catch((error) => console.log(error.message));
+}
+
+async function generateAdmin() {
+	await prisma.user.upsert({
+		where: { email: 'admin@testus.com' },
+		update: {},
+		create: {
+			firstName: 'Admin',
+			lastName: 'Nimda',
+			email: 'admin@testus.com',
+			password: await hashPassword('P0klik4$'),
+			phone: '111-222-333',
+			role: 'ADMIN',
+			isBlocked: false,
+		}
+	});
+}
+
+async function generateUsers() {
+	for (let i = 0; i < randomUsersCount; i++) {
+		await prisma.user.create({
+			data: {
+				firstName: faker.name.firstName(),
+				lastName: faker.name.lastName(),
+				email: faker.internet.email(),
+				password: await hashPassword('P0klik4$'),
+				phone: faker.phone.number('###-###-###'),
+				role: randomEnum(UserRoles),
+				isBlocked: faker.datatype.boolean(),
+			}
+		});
+	}
+}
+
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomEnum<T>(targetEnum: T, numerical = false): T[keyof T] {
+	const enumValues = Object.keys(targetEnum)
+		.map((n) => (numerical ? Number.parseInt(n) : n))
+		.filter((n) => !Number.isNaN(n)) as unknown as T[keyof T][];
+	const randomIndex = Math.floor(Math.random() * enumValues.length);
+	const randomEnumValue = enumValues[randomIndex];
+	return randomEnumValue;
+}
+
+// EXECUTE
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
